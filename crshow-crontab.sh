15e3517d778ce463fd0182f8bb6d4baa0d18231a
knightlf <knightlf@126.com>
2020-03-01 17:36:32 +0800

    add readme for tencent env

--- crshow-crontab.sh
+++ crshow-crontab.sh
@@ -3,8 +3,8 @@
 while true
 sleep 3
 do
-	/data/crshow/checkfiles rsql >>/data/logs/crshow/log-$(date "+%Y-%m-%d").log
+	/data/crshow/checkfiles rsql >>/data/logs/crshow/log-$(date +\%Y-\%m-\%d).log
 	sleep 2
-	/data/crshow/checkfiles rfile >>/data/logs/crshow/log-$(date "+%Y-%m-%d").log
+	/data/crshow/checkfiles rfile >>/data/logs/crshow/log-$(date +\%Y-\%m-\%d).log
 	sleep 2
 done
